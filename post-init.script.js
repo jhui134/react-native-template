4e4545f23bb6de4f4f9b5ce51789a21ea0e10dc4
안준혁 <44107083+jhui134@users.noreply.github.com>
2021-01-25 15:32:16 +0900

    add7

--- post-init.script.js
+++ post-init.script.js
@@ -1,3 +1,8 @@
 #!/usr/bin/env node
 
+const fs = require('fs')
+fs.copyFile('src/Config/index.js', (err) => {
+    if (err) throw err;
+    console.log('src/Config/index.js is api address good luck');
+});
 console.log("SPLIT React-Native Template initialized with success !");
